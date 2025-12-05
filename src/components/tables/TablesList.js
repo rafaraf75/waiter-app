e22b3e5a4033a667638c891e85c9c20499a3938b
Rafal Labunski <rafallabunski@gmail.com>
2025-12-05 13:52:49 +0100

    Style tables list to match design

--- src/components/tables/TablesList.js
+++ src/components/tables/TablesList.js
@@ -1,33 +1,36 @@
 // src/components/tables/TablesList.js
-import { Table, Button } from 'react-bootstrap';
+import { Button, ListGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const TablesList = ({ tables }) => {
   return (
     <>
       <h1 className="mb-4">All tables</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Status</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tables.map((table) => (
-            <tr key={table.id}>
-              <td>{table.id}</td>
-              <td>{table.status}</td>
-              <td>
-                <Button as={Link} to={`/table/${table.id}`} variant="primary">
-                  Show more
-                </Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+
+      <ListGroup>
+        {tables.map((table) => (
+          <ListGroup.Item
+            key={table.id}
+            className="d-flex justify-content-between align-items-center"
+          >
+            <div>
+              <strong>Table {table.id}</strong>
+              <span className="text-muted ms-3">
+                Status: <span>{table.status}</span>
+              </span>
+            </div>
+
+            <Button
+              as={Link}
+              to={`/table/${table.id}`}
+              variant="primary"
+              size="sm"
+            >
+              Show more
+            </Button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </>
   );
 };
